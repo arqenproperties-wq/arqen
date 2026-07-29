6b17b813766406c7595bb7229317bcec022c00a7
Sandeep Vemula <sandeepvemula408@gmail.com>
2026-07-29 10:18:13 +0530

    eden-hills

---
+++ app/valia-tower/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className="w-full h-screen">
+            <iframe
+                src="https://valia-living-edit.base44.app/"
+                className="w-full h-full border-0"
+                title="Palm Jebel Ali Lead Flow"
+                loading="lazy"
+                allow="clipboard-write; fullscreen"
+            />
+        </div>
+    )
+}
+
+export default page
