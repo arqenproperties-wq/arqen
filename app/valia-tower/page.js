889f143eaedb61c3b63fd804bcfa9a21d37dacc8
Sandeep Vemula <sandeepvemula408@gmail.com>
2026-08-05 12:19:52 +0530

    valia-tower

--- app/valia-tower/page.js
+++ app/valia-tower/page.js
@@ -4,7 +4,8 @@ const page = () => {
     return (
         <div className="w-full h-screen">
             <iframe
-                src="https://valia-living-edit.base44.app/"
+                // src="https://valia-living-edit.base44.app/"
+                src="https://thevaliaemaar.com"
                 className="w-full h-full border-0"
                 title="Palm Jebel Ali Lead Flow"
                 loading="lazy"
